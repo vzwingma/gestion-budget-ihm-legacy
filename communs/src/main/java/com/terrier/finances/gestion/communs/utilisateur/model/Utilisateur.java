2c18a199f5275eb118fc3eb551406efc7f5a9dfc
vzwingma <vincent.zwingmann@gmail.com>
2018-08-29 12:19:55 +0200

    suppression du .gitIgnore qui oubliait des classes

---
+++ communs/src/main/java/com/terrier/finances/gestion/communs/utilisateur/model/Utilisateur.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package com.terrier.finances.gestion.communs.utilisateur.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.EnumMap;
+import java.util.Map;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.terrier.finances.gestion.communs.utilisateur.enums.UtilisateurDroitsEnum;
+import com.terrier.finances.gestion.communs.utilisateur.enums.UtilisateurPrefsEnum;
+
+/**
+ * Définition d'un utilisateur de la BDD
+ * @author vzwingma
+ *
+ */
+@Document(collection = "utilisateurs")
+public class Utilisateur implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6535669274718528190L;
+	
+	@Id
+	private String id;
+	// Login
+	private String login;
+	
+	private String hashMotDePasse;
+
+	// Clé de chiffrement des données. Le mot de passe du user permet de la déchiffrer
+	@JsonIgnore
+	private String masterCleChiffrementDonnees;
+
+	@JsonIgnore	
+	private Date dernierAcces;
+	
+	// Libellé
+	private String libelle;
+	/**
+	 * Préférences
+	 */
+	@Transient
+	private transient Map<UtilisateurPrefsEnum, Object> prefsUtilisateur = new EnumMap<>(UtilisateurPrefsEnum.class);
+	/**
+	 * Droits
+	 */
+	private Map<UtilisateurDroitsEnum, Boolean> droits = new EnumMap<>(UtilisateurDroitsEnum.class);
+	
+	
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**
+	 * @param libelle the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/**
+	 * @return the hashMotDePasse
+	 */
+	public String getHashMotDePasse() {
+		return hashMotDePasse;
+	}
+
+	/**
+	 * @param hashMotDePasse the hashMotDePasse to set
+	 */
+	public void setHashMotDePasse(String hashMotDePasse) {
+		this.hashMotDePasse = hashMotDePasse;
+	}
+
+	/**
+	 * @return the prefsUtilisateur
+	 */
+	public Map<UtilisateurPrefsEnum, Object> getPrefsUtilisateur() {
+		return prefsUtilisateur;
+	}
+
+	/**
+	 * @param prefsUtilisateur the prefsUtilisateur to set
+	 */
+	public void setPrefsUtilisateur(Map<UtilisateurPrefsEnum, Object> prefsUtilisateur) {
+		this.prefsUtilisateur = prefsUtilisateur;
+	}
+
+	/**
+	 * @return the preferences
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T getPreference(UtilisateurPrefsEnum clePreference) {
+		return (T)prefsUtilisateur.get(clePreference);
+	}
+
+	/**
+	 * @return the masterCleChiffrementDonnees
+	 */
+	public String getMasterCleChiffrementDonnees() {
+		return masterCleChiffrementDonnees;
+	}
+
+	/**
+	 * @param masterCleChiffrementDonnees the masterCleChiffrementDonnees to set
+	 */
+	public void setMasterCleChiffrementDonnees(String masterCleChiffrementDonnees) {
+		this.masterCleChiffrementDonnees = masterCleChiffrementDonnees;
+	}
+
+	/**
+	 * @return the dernierAcces
+	 */
+	public Date getDernierAcces() {
+		return dernierAcces;
+	}
+
+	/**
+	 * @param dernierAcces the dernierAcces to set
+	 */
+	public void setDernierAcces(Date dernierAcces) {
+		this.dernierAcces = dernierAcces;
+	}
+
+	/**
+	 * @return the droits
+	 */
+	public Map<UtilisateurDroitsEnum, Boolean> getDroits() {
+		return droits;
+	}
+
+	/**
+	 * @param droits the droits to set
+	 */
+	public void setDroits(Map<UtilisateurDroitsEnum, Boolean> droits) {
+		this.droits = droits;
+	}
+
+	/**
+	 * @param cleDroit
+	 * @return le résultat
+	 */
+	public boolean isEnabled(UtilisateurDroitsEnum cleDroit){
+		if(this.droits != null){
+			Boolean droit = this.droits.get(cleDroit);
+			return droit != null && droit.booleanValue();
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return this.login;
+	}
+	
+	public String toFullString(){
+		StringBuilder builder = new StringBuilder();
+		builder.append("Utilisateur [login=").append(login)
+				.append(", dateDernierAcces=")
+				.append(dernierAcces != null ? dernierAcces.getTime() : "nulle").append(", libelle=").append(libelle)
+				.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ ihm/src/main/java/com/terrier/finances/gestion/ui/login/business/UserSession.java
@@ -0,0 +1,223 @@
+package com.terrier.finances.gestion.ui.login.business;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.terrier.finances.gestion.communs.budget.model.BudgetMensuel;
+import com.terrier.finances.gestion.communs.utilisateur.model.Utilisateur;
+import com.terrier.finances.gestion.ui.communs.abstrait.ui.AbstractUIController;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.server.WrappedSession;
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Window;
+
+/**
+ * Session Utilisateur
+ * @author vzwingma
+ *
+ */
+public class UserSession {
+	/**
+	 * Logger
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserSession.class);
+
+	private String idSession;
+
+	private Instant lastAccessTime;
+	
+	/**
+	 * Session Manager
+	 * @param idSession idSessions
+	 */
+	public UserSession(String idSession){
+		LOGGER.trace("[INIT][{}] Session UI ", idSession);
+		this.idSession = idSession;
+		this.lastAccessTime = Instant.now();
+	}
+
+	/**
+	 * Utilisateur courant
+	 */
+	private Utilisateur utilisateurCourant = null;
+
+	/**
+	 * Budget courant
+	 */
+	private BudgetMensuel budgetMensuelCourant;
+
+	@SuppressWarnings("rawtypes")
+	private Map<Class, AbstractUIController<? extends AbstractComponent>> mapControleurs = new HashMap<>();
+	// Page principale
+	private Layout mainLayout;
+
+	private Window popupModale;
+
+
+	/**
+	 * Déconnexion de l'utilisateur manuellement
+	 */
+	public void deconnexion(){
+		// Déco auto
+		autoDeconnexion();
+		//Redirect the user to the login/default Page
+		Page currentPage = Page.getCurrent();
+		VaadinServlet currentServlet = VaadinServlet.getCurrent();
+		if(currentPage != null && currentServlet != null && currentServlet.getServletContext() != null && currentServlet.getServletContext().getContextPath() != null){
+			currentPage.setLocation(currentServlet.getServletContext().getContextPath()+"/ihm");
+		}
+		else{
+			LOGGER.error("Erreur : Impossible de trouver la page courante. Pb de framework Vaadin");
+		}
+	}
+
+	/**
+	 * Auto déconnexion, sans redirection
+	 */
+	public void autoDeconnexion(){
+		// Suppression de l'utilisateur
+		this.utilisateurCourant = null;
+		this.lastAccessTime = null;
+		// Suppression de l'IHM
+		getMainLayout().removeAllComponents();
+		// Suppression de tous les controleurs
+		mapControleurs.clear();
+
+		// Invalidate Sessions
+		VaadinSession vSession = VaadinSession.getCurrent();
+		if(vSession != null){
+			vSession.close();
+			WrappedSession httpSession = vSession.getSession();
+			//Invalidate HttpSession
+			httpSession.invalidate();
+		}
+		
+	}
+
+
+	/**
+	 * Enregistrement des controleurs
+	 * @param controleur controleur à enregistrer
+	 */
+	public <C extends AbstractUIController<? extends AbstractComponent>> void registerUIControler(C controleur) {
+		if(mapControleurs.get(controleur.getClass()) == null){
+			LOGGER.info("[{}] Enregistrement du controleur : {}", this.idSession, controleur.getClass().getSimpleName());
+			mapControleurs.put(controleur.getClass(), controleur);
+		}
+
+	}
+
+
+
+	/**
+	 * @return the mapControleurs
+	 */
+	@SuppressWarnings("unchecked")
+	public <C extends AbstractUIController<? extends CustomComponent>> C getControleur(Class<C> classNameControleur) {
+		return (C) mapControleurs.get(classNameControleur);
+	}
+
+	/**
+	 * Enregistrement de l'utilisateur
+	 * @param utilisateur USER
+	 */
+	public boolean registerUtilisateur(Utilisateur utilisateur){
+		LOGGER.info("[{}] Enregistrement de l'utilisateur : {}", idSession, utilisateur);
+		this.utilisateurCourant = utilisateur;
+		return true;
+	}
+
+
+	/**
+	 * @return the utilisateurCourant
+	 */
+	public Utilisateur getUtilisateurCourant() {
+		return utilisateurCourant;
+	}
+
+
+
+	/**
+	 * @param lastAccessTime the lastAccessTime to set
+	 */
+	public void setLastAccessTime(Instant lastAccessTime) {
+		this.lastAccessTime = lastAccessTime;
+	}
+
+
+	/**
+	 * @return the lastAccessTime
+	 */
+	public Instant getLastAccessTime() {
+		return lastAccessTime;
+	}
+
+
+	/**
+	 * @return session active
+	 */
+	public boolean isActive(){
+		return utilisateurCourant != null && this.budgetMensuelCourant != null;
+	}
+
+	/**
+	 * @return the idSession
+	 */
+	public String getIdSession() {
+		return idSession;
+	}
+
+	/**
+	 * @return the budgetMensuelCourant
+	 */
+	public BudgetMensuel getBudgetMensuelCourant() {
+		return budgetMensuelCourant;
+	}
+
+	/**
+	 * @param budgetMensuelCourant the budgetMensuelCourant to set
+	 */
+	public void setBudgetMensuelCourant(BudgetMensuel budgetMensuelCourant) {
+		this.budgetMensuelCourant = budgetMensuelCourant;
+	}
+
+
+
+	/**
+	 * @return le mainLayout
+	 */
+	public Layout getMainLayout() {
+		return mainLayout;
+	}
+
+	/**
+	 * @param mainLayout to set
+	 */
+	public void setMainLayout(Layout mainLayout) {
+		this.mainLayout = mainLayout;
+	}
+
+	/**
+	 * @return the popupModale
+	 */
+	public Window getPopupModale() {
+		return popupModale;
+	}
+
+	/**
+	 * @param popupModale the popupModale to set
+	 */
+	public void setPopupModale(Window popupModale) {
+		UI.getCurrent().addWindow(popupModale);
+		this.popupModale = popupModale;
+	}
+}

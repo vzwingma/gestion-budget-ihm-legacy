c71c77d0ced49b6acb1a313c3d44eda84ec57bdc
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-13 11:22:52 +0200

    suppression d'import

--- src/main/java/com/terrier/finances/gestion/ui/operations/creation/ui/CreerDepenseController.java
+++ src/main/java/com/terrier/finances/gestion/ui/operations/creation/ui/CreerDepenseController.java
@@ -1,7 +1,6 @@
 package com.terrier.finances.gestion.ui.operations.creation.ui;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

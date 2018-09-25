7052ed02ee9bdedfb2422727061a40f460a715fd
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-25 16:37:23 +0200

    correction TU

--- src/test/java/com/terrier/finances/gestion/test/config/AbstractTestServices.java
+++ src/test/java/com/terrier/finances/gestion/test/config/AbstractTestServices.java
@@ -5,25 +5,20 @@ package com.terrier.finances.gestion.test.config;
 
 import static org.mockito.Mockito.spy;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.terrier.finances.gestion.services.operations.api.OperationsAPIService;
 import com.terrier.finances.gestion.services.parametrages.api.ParametragesAPIService;
-import com.terrier.finances.gestion.ui.communs.config.AppConfig;
 
 /**
  * Classe abstraite des tests d'API
  * @author vzwingma
  *
  */
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes={AppConfig.class})
+
+
 public abstract class AbstractTestServices {
 
 	/**
@@ -31,11 +26,10 @@ public abstract class AbstractTestServices {
 	 */
 	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
-	@Autowired
-	private ParametragesAPIService paramsAPIService;
-	
-	@Autowired
-	private OperationsAPIService operationsAPIService;
+
+	private ParametragesAPIService paramsAPIService = new ParametragesAPIService();
+
+	private OperationsAPIService operationsAPIService = new OperationsAPIService();
 	
 	@Bean public ParametragesAPIService spyParamsAPIService(){
 		return spy(paramsAPIService);

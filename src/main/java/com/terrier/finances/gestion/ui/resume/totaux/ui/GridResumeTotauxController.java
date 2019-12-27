ae023ee7583d4c382ca92e07fe846736a94c4866
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2019-12-27 18:23:27 +0100

    suppression de la ligne marge sur l'IHM

--- src/main/java/com/terrier/finances/gestion/ui/resume/totaux/ui/GridResumeTotauxController.java
+++ src/main/java/com/terrier/finances/gestion/ui/resume/totaux/ui/GridResumeTotauxController.java
@@ -56,7 +56,7 @@ public class GridResumeTotauxController extends AbstractUIController<GridResumeT
 		
 		// Injection des données
 		List<TotalBudgetMensuel> totauxBudget = new ArrayList<>();
-		totauxBudget.add(new TotalBudgetMensuel("Solde prévu", budget.getSoldeNow(), budget.getSoldeFin()));
+		totauxBudget.add(new TotalBudgetMensuel("Solde", budget.getSoldeNow(), budget.getSoldeFin()));
 		// Maj des colonnes
 		getComponent().getColumn(EntetesGridResumeOperationsEnum.VALEUR_NOW.getId()).setCaption(EntetesGridResumeOperationsEnum.VALEUR_NOW.getLibelle()+ dateDerniereOperation.format(auDateFormat));
 		getComponent().getColumn(EntetesGridResumeOperationsEnum.VALEUR_FIN.getId()).setCaption(EntetesGridResumeOperationsEnum.VALEUR_FIN.getLibelle()+ dateDerniereOperation.format(finDateFormat));
